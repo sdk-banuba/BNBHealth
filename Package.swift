2f5e1b176b99a7d055c2a02a8e34b3bb9f80eb30
ci <ci@banuba.com>
2024-06-05 15:27:23 +0300

    1.13.2-44-g649e5173b

---
+++ Package.swift
@@ -0,0 +1,84 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.13.2-44-g649e5173b"
+
+let package = Package(
+    name: "BNBHealth",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBHealth",
+            targets: [
+                "BNBHealth",
+                "BNBHealth_BNBSdkCore",
+                "BNBHealth_BNBEffectPlayer",
+                "BNBHealth_BNBScripting",
+                "BNBHealth_BNBLips"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/sdk-banuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBEffectPlayer.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBScripting.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBLips.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBHealth",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.13.2-44-g649e5173b/BNBHealth.zip",
+            checksum: "d1dc8896fd4e26a86b97a4c8e1421a664b1cda6e7a32794324e23e61662012f5"
+        ),
+        .target(
+            name: "BNBHealth_BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBHealth_BNBEffectPlayer",
+            dependencies: [
+                .product(
+                    name: "BNBEffectPlayer",
+                    package: "BNBEffectPlayer"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBHealth_BNBScripting",
+            dependencies: [
+                .product(
+                    name: "BNBScripting",
+                    package: "BNBScripting"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBHealth_BNBLips",
+            dependencies: [
+                .product(
+                    name: "BNBLips",
+                    package: "BNBLips"
+                ),
+            ]
+        ),
+    ]
+)
