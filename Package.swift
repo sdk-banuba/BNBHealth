589c67f273a525aef9bb828b7375bbbee18c84d4
ci <ci@banuba.com>
2025-12-19 11:56:20 +0300

    1.17.6-65-gbf994362d5

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.17.6"
+let bnbPackageVersion: Version = "1.17.6-65-gbf994362d5"
 
 let package = Package(
     name: "BNBHealth",
@@ -41,8 +41,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBHealth",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.17.6/BNBHealth.zip",
-            checksum: "bcd36a8fae8cc6a87a7770bae6e7557694e2085df81b7abcb0e1a16b3919b467"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.17.6-65-gbf994362d5/BNBHealth.zip",
+            checksum: "08c3e9ac3e0487825926237acaa63e6552d27786a7738a8de8a46fa1221be158"
         ),
         .target(
             name: "BNBHealth_BNBSdkCore",
